634f212f67711fe324ed4bfb0f4c83ce7313f9b0
bourrasque <stampixel.t@gmail.com>
2024-08-27 17:17:20 -0400

    Base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App bg-lavender_blush font-satoshi overflow-x-hidden">
+            <section id={"home"}>
+                <div className={"h-lvh flex"}>
+                    <div className={"m-auto flex flex-col gap-y-3"}>
+                        <h1 className={"font-black text-8xl"}>Alice</h1>
+                        <h2 className={"font-black text-8xl"}>Hacks</h2>
+                        <h3 className={"font-bold text-2xl"}>November 17-19, 2024</h3>
+                        <a href={"google.com"} className={""}>
+                            <button className="h-14 border-black text-xl font-bold border-2 p-2.5 bg-beige transition-all hover:bg-rosy_brown shadow-[4px_4px_0px_rgba(0,0,0,1)] hover:shadow-none active:bg-[#00E1EF] rounded-md">
+                                Register Soon!
+                            </button>
+                        </a>
+
+                    </div>
+                    <div>
+
+                    </div>
+
+                </div>
+
+                <div id={"autoScrollContainer"} className={"w-full overflow-hidden  border-y-4"}>
+                    <div id={"autoScrollText"}>
+                        <p className={"text-nowrap text-5xl font-bold"}>Alice Hack Alice Hack Alice Hack Alice Hack</p>
+                    </div>
+
+                </div>
+
+
+            </section>
+
+            <section id={"about us"}>
+
+            </section>
+
+            <section id={"sponsorStats"}>
+
+            </section>
+
+            <section id={"sponsorEvent"}>
+
+            </section>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        colors: {
+            black: '#1d1d1d',
+            dark_grey: '#33383f',
+            light_grey: '#99a4af',
+            grey: '#626a72',
+            gradient_red: '#ff010126',
+            back_grey: '#f5f6f7',
+            misty_rose: '#ffdada',
+            lavender_blush: '#ffefef',
+            gradient_blue: '#7aa7ff52',
+            dim_grey: '#495158',
+            silver: '#e7ecf0',
+            soft_grey: '#cbd5df',
+            gradient_pink: '#ef98cf40',
+            dark_grey_2: '#3a3a3a',
+            beige: '#fffef9',
+            rosy_brown: '#b89797',
+            cornflower_blue: '#d4e2ff',
+            misty_rose_2: '#f78586',
+            navajo_white: '#ffcd82',
+            black_2: '#1d1d1d',
+            navajo_white_2: '#ffe2b8',
+            lavender_blush_2: '#b9b9d1',
+            rosy: '#b99898'
+        },
+        extend: {
+            fontFamily: {
+                'satoshi': ['Satoshi']
+            },
+        },
+    },
+    plugins: [],
+}
+
