5efa0de9dbd6758749b0674a2c633f72a866c631
bourrasque <stampixel.t@gmail.com>
2024-08-30 18:08:49 -0400

    Logo recoded

--- src/App.js
+++ src/App.js
@@ -16,28 +16,38 @@ function App() {
                                 Register Soon!
                             </button>
                         </a>
-
                     </div>
                     <div>
-
-                    </div>
-
-                </div>
-
-                <div id={"autoScrollContainer"} className={"h-24 w-full overflow-hidden border-y-4 border-black flex bg-white"}>
-                    <div id={"autoScrollText"} className={"my-auto"}>
-                        {/*https://blog.hubspot.com/website/scrolling-text-css*/}
-                        {/*Make text wrap better*/}
-                        <p className={"text-nowrap text-7xl font-bold tracking-tighter"}>Alice Hack! Alice Hack! Alice
-                            Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack!</p>
                     </div>
                 </div>
+
+                {/*<div id={"autoScrollContainer"}*/}
+                {/*     className={"h-24 w-full overflow-hidden border-y-4 border-black flex bg-white"}>*/}
+                {/*    <div id={"autoScrollText"} className={"my-auto"}>*/}
+                {/*        /!*https://blog.hubspot.com/website/scrolling-text-css*!/*/}
+                {/*        /!*Make text wrap better*!/*/}
+                {/*        <p className={"text-nowrap text-7xl font-bold tracking-tighter"}>Alice Hack! Alice Hack! Alice*/}
+                {/*            Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice*/}
+                {/*            Hack!</p>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </section>
 
-            <section id={"aboutUs"} className={"h-screen flex"}>
+            <div id={"autoScrollContainer"}
+                 className={"h-24 w-full overflow-hidden border-y-4 border-black flex bg-white"}>
+                <div id={"autoScrollText"} className={"my-auto"}>
+                    {/*https://blog.hubspot.com/website/scrolling-text-css*/}
+                    {/*Make text wrap better*/}
+                    <p className={"text-nowrap text-7xl font-bold tracking-tighter"}>Alice Hack! Alice Hack! Alice
+                        Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack! Alice Hack!</p>
+                </div>
+            </div>
+
+            <section id={"aboutUs"} className={"h-screen flex bg-misty_rose"}>
                 <div className={"m-auto"}>
-                    <div className={"m-auto flex flex-col w-4/5"}>
-                        <div className={"z-10 -mx-4 -mb-2 w-3/5 p-3 bg-cornflower_blue border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] slow-shift"}>
+                    <div className={"m-auto flex flex-col w-4/5 md:w-3/5"}>
+                        <div
+                            className={"z-10 -mx-4 -mb-2 w-3/5 p-3 bg-cornflower_blue border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] slow-shift"}>
                             <div className={""}>
                                 <h3>ABOUT US</h3>
                             </div>
@@ -59,20 +69,19 @@ function App() {
                 </div>
             </section>
 
-            <section id={"sponsorStats"} className={"h-screen flex border-y-4 border-black bg-misty_rose"}>
+            <section id={"sponsorStats"} className={"h-[42rem] flex border-y-4 border-black md:h-screen"}>
                 <div className={"relative w-full h-full"}>
-                    {/*centers because of negative margins*/}
-                    <img className={"absolute w-6/12 left-0 right-0 top-11 mx-auto"} alt={""} src={"/schools.png"}/>
-                    <img className={"absolute w-6/12 left-4 top-40"} alt={""} src={"/schools.png"}/>
-                    <img className={"absolute w-6/12 right-8 top-64"} alt={""} src={"/schools.png"}/>
-                     {/*Change this to the 2 cheering bunny*/}
-                    <img className={"absolute w-8/12 top-2/4 mx-auto left-0 right-0"} alt={""} src={"/bunny_cheer.png"}/>
+                    <img className={"absolute w-6/12 left-0 right-0 top-11 mx-auto md:w-3/12"} alt={""} src={"/schools.png"}/>
+                    <img className={"absolute w-6/12 left-4 top-40 md:w-3/12"} alt={""} src={"/schools.png"}/>
+                    <img className={"absolute w-6/12 right-8 top-64 md:w-3/12"} alt={""} src={"/schools.png"}/>
+                    <img className={"absolute w-8/12 bottom-16 mx-auto left-0 right-0 md:w-5/12"} alt={""}
+                         src={"/bunny_cheer.png"}/>
                 </div>
             </section>
 
             <section id={"sponsorEvent"} className={"h-[32rem] flex"}>
                 <div
-                    className={"gap-y-2 m-auto w-11/12 px-8 py-4 bg-white border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] grid place-content-center"}>
+                    className={"gap-y-2 m-auto w-11/12 px-8 py-4 bg-white border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] grid place-content-center md:w-8/12"}>
                     {/*<h1 className={"text-4xl font-black"}>Sponsor an Event of ${`{}`}</h1>*/}
                     <h1 className={"text-4xl font-black"}>Sponsor an Event of <h1 className={"text-gradient_blue"}>
                         <Typing/></h1></h1>
@@ -97,59 +106,53 @@ function App() {
                     <div className={"w-11/12 md:w-3/5 m-auto"}>
                         <div className={"flex flex-col"}>
                             <div
-                                className={"h-[10rem] mb-4 px-12 py-4 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://www.bestbuy.com"} className={"h-full flex"}>
+                                className={"mb-4 flex justify-center items-center"}>
+                                <a href={"https://www.bestbuy.com"}
+                                   className={"flex justify-center items-center w-full h-[20vw] bg-white border-2 border-black transition-all hover:translate-y-0.5 hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] hover:shadow-none rounded-md"}>
                                     <img
                                         src="/logos/BEST_BUY.png" alt={""}
-                                        className={"object-scale-down h-full"}/>
+                                        className={"max-h-[60%] max-w-[60%]"}/>
+                                </a>
+                            </div>
+                            <div
+                                className={"mb-4 flex justify-center items-center"}>
+                                <a href={"https://www.bestbuy.com"}
+                                   className={"flex justify-center items-center w-full h-[20vw] bg-white border-2 border-black transition-all hover:translate-y-0.5 hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] hover:shadow-none rounded-md"}>
+                                    <img
+                                        src="/logos/GAR.png" alt={""}
+                                        className={"max-h-[60%] max-w-[60%]"}/>
+                                </a>
+                            </div>
+
+                        </div>
+                        <div className={"grid grid-cols-2 gap-4 place-items-center"}>
+                            <div
+                                className={"justify-center items-center"}>
+                                <a href={"https://www.bestbuy.com"}
+                                   className={" justify-center items-center w-full h-[20vw] bg-white border-2 border-black transition-all hover:translate-y-0.5 hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid hover:shadow-none rounded-md"}>
+                                    <img
+                                        src="/logos/GAR.png" alt={""}
+                                        className={"max-h-[60%] max-w-[60%] m-auto"}/>
                                 </a>
                             </div>
 
                             <div
-                                className={"h-[10rem] mb-4 px-12 py-4 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://automationroboticsarduino.com"} className={"h-full w-full flex"}><img
-                                    src="/logos/GAR.png" alt={""} className={"object-scale-down mx-auto"}/></a>
-                            </div>
-                        </div>
-                        <div className={"grid grid-cols-2 gap-4 place-items-center"}>
-                            <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://certopus.com/"} className={"h-full w-full flex"}><img
-                                    src="/logos/CERTOPUS.svg" alt={""}
-                                    className={"object-scale-down mx-auto"}/></a>
-                            </div>
-                            <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://igda.org/"} className={"h-full w-full flex"}>
+                                className={"justify-center items-center"}>
+                                <a href={"https://www.bestbuy.com"}
+                                   className={" justify-center items-center w-full h-[20vw] bg-white border-2 border-black transition-all hover:translate-y-0.5 hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid hover:shadow-none rounded-md"}>
                                     <img
-                                        src="/logos/IGDA.png" alt={""}
-                                        className={"object-scale-down mx-auto"}/>
+                                        src="/logos/GAR.png" alt={""}
+                                        className={"max-h-[60%] max-w-[60%] m-auto"}/>
                                 </a>
                             </div>
                             <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a className={"h-full w-full flex"}><img
-                                    src="/logos/INTERVIEW_BUDDY.png" alt={""}
-                                    className={"object-scale-down mx-auto"}/></a>
-                            </div>
-                            <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://interviewbuddy.net/"} className={"h-full w-full flex"}><img
-                                    src="/logos/NORDVPN.png" alt={""}
-                                    className={"object-scale-down mx-auto"}/></a>
-                            </div>
-                            <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://www.oreilly.com/"} className={"h-full w-full flex"}><img
-                                    src="/logos/OREILLY.png" alt={""}
-                                    className={"object-scale-down mx-auto"}/></a>
-                            </div>
-                            <div
-                                className={"h-[7rem] px-8 py-8 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                                <a href={"https://gen.xyz/"} className={"h-full w-full flex"}>
+                                className={"justify-center items-center"}>
+                                <a href={"https://www.bestbuy.com"}
+                                   className={" justify-center items-center w-full h-[20vw] bg-white border-2 border-black transition-all hover:translate-y-0.5 hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid hover:shadow-none rounded-md"}>
                                     <img
-                                        src="/logos/XYZ.png" alt={""}
-                                        className={"object-scale-down mx-auto"}/></a>
+                                        src="/logos/GAR.png" alt={""}
+                                        className={"max-h-[60%] max-w-[60%] m-auto"}/>
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -160,13 +163,13 @@ function App() {
                     <h3 className={"text-3xl font-black pt-12 pb-6"}>Our partners</h3>
                 </div>
 
-                <div className={"flex flex-col w-11/12 m-auto"}>
+                <div className={"w-11/12 md:w-3/5 m-auto"}>
                     <div
                         className={"h-[10rem] mb-4 px-12 py-4 bg-white border-2 border-black transition-all hover:shadow-none hover:bg-rosy_brown shadow-[6px_6px_0px_rgba(0,0,0,1)] grid place-content-center"}>
-                        <a href={"https://www.bestbuy.com"} className={"h-full w-full flex"}>
+                        <a href={"https://www.bestbuy.com"} className={"h-full flex"}>
                             <img
                                 src="/logos/BEST_BUY.png" alt={""}
-                                className={"object-scale-down mx-auto"}/>
+                                className={"object-scale-down h-full"}/>
                         </a>
                     </div>
                 </div>
